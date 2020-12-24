4b91d1965c8de10b8945fc9651fe2879159216f0
kartikkanoo <kartikkanoo@gmail.com>
2020-12-24 20:40:30 +0800

    2015

--- week04/scripts.js
+++ week04/scripts.js
@@ -5,3 +5,15 @@ function transformUppercase() {
 
 let elUsername = document.getElementById("namaPengguna");
 elUsername.addEventListener("keyup", transformUppercase);
+
+function checkPasswordLength(){
+    let password = document.getElementById("katalaluan").value;
+    if(password.length < 5) {
+        alert("Password must be more than 5 characters");
+    } else {
+        alert("Password accepted!")
+    }
+}
+
+let elPassword = document.getElementById("kataLaluan");
+elPassword.onblur = checkPasswordLength;
