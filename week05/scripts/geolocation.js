2de2e95d9d89de28e1a3249cbfe63c699f02ca46
kartikkanoo <kartikkanoo@gmail.com>
2020-12-25 13:15:08 +0800

    2019

--- week05/scripts/geolocation.js
+++ week05/scripts/geolocation.js
@@ -5,8 +5,8 @@ elLocate.addEventListener("click",function(){
             let elLat = document.getElementById("lat");
             let elLong = document.getElementById("long")
 
-            elLat.innerHTML = "Latitude : " + CaretPosition.coords.latitude;
-            elLong.innerHTML = "Longitude : " + CaretPosition.coords.longitude;
+            elLat.innerHTML = "Latitude : " + position.coords.latitude;
+            elLong.innerHTML = "Longitude : " + position.coords.longitude;
          });
     } else {
         alert("Geolocation is not supported by this browser.")
