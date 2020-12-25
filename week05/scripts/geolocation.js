4034698de4421fc3dce7430b93c094491de52c22
kartikkanoo <kartikkanoo@gmail.com>
2020-12-25 13:24:02 +0800

    2020

--- week05/scripts/geolocation.js
+++ week05/scripts/geolocation.js
@@ -1,9 +1,9 @@
 let elLocate = document.getElementById("locate");
-elLocate.addEventListener("click",function(){
+elLocate.addEventListener("click", function(){
     if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(){
+        navigator.geolocation.getCurrentPosition(function(position) {
             let elLat = document.getElementById("lat");
-            let elLong = document.getElementById("long")
+            let elLong = document.getElementById("long");
 
             elLat.innerHTML = "Latitude : " + position.coords.latitude;
             elLong.innerHTML = "Longitude : " + position.coords.longitude;
