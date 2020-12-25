0c0634a72284eb0f2682b24ec1c177ad2bbf48dd
kartikkanoo <kartikkanoo@gmail.com>
2020-12-26 04:55:10 +0800

    2022

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -14,6 +14,10 @@ function getRandomUser() {
            elFirstName.innerHTML = data.results[0].name.first;
            elUserImage.src = data.result[0].picture.large;
            elUserImage.title = data.results[0].name.first + " " + data.result[0].name.last;
+           elLastName.innerHTML = data.results[0].name.last;
+           elPhone.innerHTML = data.results[0].phone;
+           elAddress.innerHTML = data.results[0].location.street.number + ", " + data.results[0].location.street.name + ", <br>" + data.results[0].location.postcode + " " + " " + data.results[0].location.city + ", <br>" + data.results[0].location.country;
+           elEmail.innerHTML = data.results[0].email;
 
            elJsonResult.innerHTML = this.response;
        } 
