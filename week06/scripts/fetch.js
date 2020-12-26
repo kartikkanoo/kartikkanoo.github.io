80710e691daaf14cbc08ecc9682d106aa94ecd9b
kartikkanoo <kartikkanoo@gmail.com>
2020-12-27 02:49:47 +0800

    2007

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -26,3 +26,35 @@ function GetBookings() {
 let getBookingBtn = document.getElementById("getBooking"); getBookingBtn.addEventListener("click", function () {
     GetBookings();
 });
+
+function BookNow(guestName, guestEmail, guestPax) {
+    let url = 'https://api.sheety.co/3578597d1c6f5275521223b092a2714d/bookingApp/bookings';
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body)
+    })
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with object
+            //console.log(json.booking);
+            let bookMsg = document.getElementById("bookMsg");
+            bookMsg.innerHTML = json.booking.name + " added!"
+            GetBookings();
+        });
+}
+
+let bookNow = document.getElementById("bookNow");
+bookNow.addEventListener("click", function () {
+    let gName = document.getElementById("guestName").Value;
+    let gEmail = document.getElementById("guestEmail").value;
+    let gPax = document.getElementById("guestPax").value;
+
+    BookNow(gName, gEmail, gPax);
+});
