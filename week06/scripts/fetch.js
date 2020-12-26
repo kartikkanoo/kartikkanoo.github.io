1e7e340e2577e35d1de1ead6754b3e1e10f11cb9
kartikkanoo <kartikkanoo@gmail.com>
2020-12-27 02:59:03 +0800

    2008

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -38,7 +38,10 @@ function BookNow(guestName, guestEmail, guestPax) {
     }
     fetch(url, {
         method: 'POST',
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
     })
         .then((response) => response.json())
         .then(json => {
