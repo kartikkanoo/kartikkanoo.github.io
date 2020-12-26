668bcd34ef554c4cb5432b7c06b85268e2dcb753
kartikkanoo <kartikkanoo@gmail.com>
2020-12-27 01:28:42 +0800

    2002

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -5,6 +5,16 @@ function GetBookings() {
         .then(json => {
             // Do something with the data
             console.log(json.bookings);
+            let bookingList = document.getElementById("getBookingList");
+            bookingList.innerHTML = "";
+            for (let i = 0; i < json.bookings.length; i++) {
+                let gName = json.bookings[i].name;
+                let gEmail = json.bookings[i].email;
+                let gPax = json.bookings[i].pax;
+                let gId = json.bookings[i].id;
+
+                bookingList.innerHTML += gId + "-" + gName + ", " + gEmail + ", pax:" + pax + "<br>";
+            }
         });
 }
 
