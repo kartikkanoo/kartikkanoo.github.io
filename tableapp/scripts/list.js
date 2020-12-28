c9ba72995f050f1f7f492611a871c8f0c937bf28
kartikkanoo <kartikkanoo@gmail.com>
2020-12-28 13:31:34 +0800

    2002

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -8,6 +8,11 @@ function GetBookings() {
             let bookingList = document.getElementById("getbookingList");
             let bookingIds = [];
 
+            //clear the table rows
+            for (let k = bookingList.rows.length - 1; k > 0; k--) {
+                bookingList.deleteRow(k);
+            }
+
             for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
@@ -22,7 +27,8 @@ function GetBookings() {
                 row.insertCell(2).innerHTML = gEmail;
                 row.insertCell(3).innerHTML = gPax;
                 row.insertCell(4).innerHTML = gRemarks;
-                row.insertCell(5).innerHTML = "<button id='" + buttonId + "' class='btn btn-danger'>Delete</button>";
+                row.insertCell(5).innerHTML = "<button id='" +
+                    buttonId + "' class='btn btn-danger'>Delete</button>";
 
                 bookingIds.push(buttonId);
             }
