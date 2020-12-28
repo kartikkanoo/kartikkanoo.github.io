7cc233ec5e2e4491af8cfa1af89ba96696838add
kartikkanoo <kartikkanoo@gmail.com>
2020-12-28 16:14:00 +0800

    2004

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -8,11 +8,6 @@ function GetBookings() {
             let bookingList = document.getElementById("booking-List");
             let bookingIds = [];
 
-            //clear the table rows
-            for (let k = bookingList.rows.length - 1; k > 0; k--) {
-                bookingList.deleteRow(k);
-            }
-
             for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
